a909d469dc713c1b0dec23fed2cd05f4fbbd80b4
Tamias <n.tamia5@icloud.com>
2024-01-13 21:09:29 +0200

    up

--- models/Contact.js
+++ models/Contact.js
@@ -30,7 +30,6 @@ export const addContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
